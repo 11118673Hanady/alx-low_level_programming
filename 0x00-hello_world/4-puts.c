224f1d85ce84394e0b1d404e642c96b9d505a8a0
11118673Hanady <dr_hanady37@hotmail.com>
2022-12-08 07:27:39 -0800

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,11 @@
 #include <stdio.h>
-
 /**
- *
- *  * main - main block
- *
- *   * Return: 0
- *
- *    */
-
+* main - entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
-
 {
-
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-
-	return (0);
-
+         puts("/"Programming is like building a multilingual puzzle");
+         return (0);
 }
